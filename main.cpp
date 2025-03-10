6241cdd2bd870b748209f752edbc95a677bb2479
安久琲奈/LunaticGhoulPiano <38567204+LunaticGhoulPiano@users.noreply.github.com>
2025-03-10 12:43:47 +0800

    Save progress

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <exception>
 #include <vector>
-#include <stack>
 #include <string>
-//#include <sstream>
 #include <unordered_map>
 #include <memory>
 
 std::string gTestNum; // note that it is int + '\n'
 
-/* Token Types*/
+/* Temporary structure for judging S-Exp with multiple line-returns */
+typedef struct TempTokenInLineBuffer {
+    std::string value = "";
+    int lineNum = -1;
+    int columnNum = -1;
+} TempTokenInLineBuffer;
+
+/* Token Types */
 enum class TokenType {
     LEFT_PAREN, // '('
     RIGHT_PAREN, // ')'
@@ -32,17 +37,6 @@ enum class TokenType {
 struct Token {
     TokenType type;
     std::string value = "";
-    int line = -1;
-    int column = -1;
-    void reset() {
-        value = "";
-        line = -1;
-        column = -1;
-    }
-    void setPos(int lineNum, int columnNum) {
-        line = lineNum;
-        column = columnNum;
-    }
 };
 
 /* S-Expression Abstract Syntax Tree */
@@ -127,30 +121,30 @@ class CorrectExit: public BaseException {
 class UnexpectedToken: public BaseException {
     public:
         UnexpectedToken(int line, int column, const std::string &token):
-            BaseException("> ERROR (unexpected token) : atom or '(' expected when token at Line "
-                + std::to_string(line) + " Column " + std::to_string(column) + " is >>" + token + "<<\n\n") {}
+            BaseException("\n> ERROR (unexpected token) : atom or '(' expected when token at Line "
+                + std::to_string(line) + " Column " + std::to_string(column) + " is >>" + token + "<<\n") {}
 };
 
 // ERROR (unexpected token) : ')' expected when token at Line X Column Y is >>...<<
 class NoRightParen: public BaseException {
     public:
         NoRightParen(int line, int column, const std::string &token):
-            BaseException("> ERROR (unexpected token) : ')' expected when token at Line "
-                + std::to_string(line) + " Column " + std::to_string(column) + " is >>" + token + "<<\n\n") {}
+            BaseException("\n> ERROR (unexpected token) : ')' expected when token at Line "
+                + std::to_string(line) + " Column " + std::to_string(column) + " is >>" + token + "<<\n") {}
 };
 
 // ERROR (no closing quote) : END-OF-LINE encountered at Line X Column Y
 class NoClosingQuote: public BaseException {
     public:
         NoClosingQuote(int line, int column):
-            BaseException("> ERROR (no closing quote) : END-OF-LINE encountered at Line "
-                + std::to_string(line) + " Column " + std::to_string(column) + "\n\n") {}
+            BaseException("\n> ERROR (no closing quote) : END-OF-LINE encountered at Line "
+                + std::to_string(line) + " Column " + std::to_string(column) + "\n") {}
 };
 
 // ERROR (no more input) : END-OF-FILE encountered
 class NoMoreInput: public BaseException {
     public:
-        NoMoreInput(): BaseException("> ERROR (no more input) : END-OF-FILE encountered\n\n") {}
+        NoMoreInput(): BaseException("\n> ERROR (no more input) : END-OF-FILE encountered\n") {}
 };
 
 /* S-Expression Lexer */
@@ -199,36 +193,35 @@ class S_Exp_Lexer {
         }
 
         void printAllTokens() {
-            for (auto token: tokens) std::cout << "token: " << token.value << " at Line " << token.line << " Column " << token.column << std::endl;
+            for (auto token: tokens) std::cout << "token: " << token.value << std::endl;
         }
 
         void saveAToken(Token &token) {
-            if (token.value == "") return;
-            judgeTokenType(token);
-            std::cout << token.value << "\n" << std::endl;
             //std::cout << "save token: ->" << token.value << "<- at Line " << token.line << " Column " << token.column << std::endl;
             tokens.push_back(token);
-            token.reset();
+            token.value = "";
+            lineNum = 1;
+            columnNum = 0;
+
+            // just to correctly quit the system
             int s = tokens.size();
             if (s >= 3 && tokens[s-3].value == "(" && tokens[s-2].value == "exit" && tokens[s-1].value == ")") throw CorrectExit();
         }
 
         void read() {
             Token token; // to store single token
-            std::vector<Token> lineBuffer; // clear when line error or save when line ok
+            std::vector<TempTokenInLineBuffer> lineBuffer; // clear when line error or save when line ok
             lineNum = 1;
             columnNum = 0;
             ch = '\0';
             prev_ch = '\0';
-            if (printInputSign) std::cout << "> ";
+            if (printInputSign) std::cout << "\n0> "; // input prompt
             printInputSign = true;
 
             while (std::cin.get(ch)) {
-                std::cout << "->" << ch << "<-" << std::endl;
                 if (ch == ';') {
                     if (! token.value.empty()) {
                         if (token.value[0] != '\"') {
-                            // saveAToken(token);
                             while (ch != '\n') std::cin.get(ch);
                         }
                         else { // in the double-quote
@@ -236,7 +229,7 @@ class S_Exp_Lexer {
                             columnNum++;
                         }
                     }
-                    else while (ch != '\n') std::cin.get(ch);
+                    else while (std::cin.peek() != '\n') std::cin.get(ch);
                 }
                 else if (isWhiteSpace(ch)) {
                     if (token.value.empty()) { // between complete tokens
@@ -244,31 +237,35 @@ class S_Exp_Lexer {
                             lineNum++;
                             columnNum = 0;
                         }
-                        else columnNum++;
+                        else columnNum++; // skip ' ' or '\t'
                     }
                     else { // incomplete token still inputing
                         if (ch == '\n') {
-                            switch (token.value[0]) {
-                                case '\"': {
-                                    columnNum++;
-                                    throw NoClosingQuote(token.line, token.column);
-                                }
-                                case '(': {
-                                    // because still not meat the coressponding ')'
-                                    // so only a line-feed, still inputing a double-quote
-                                    lineNum++;
-                                    columnNum = 0;
-                                }
-                                case '\'': {
-                                    // because still not meat the end of single-quote (ATOM or RIGHT-PAREN)
-                                    // so only a line-feed, still inputing a double-quote
-                                    lineNum++;
-                                    columnNum = 0;
-                                }
+                            if (token.value[0] == '\"') {
+                                columnNum++;
+                                throw NoClosingQuote(lineNum, columnNum);
+                            }
+                            else {
+                                // may have error here
+                                // because still not meat the coressponding ')'
+                                // so only a line-feed, still inputing a double-quote
+                                std::cout << "\n1> " << token.value << "\n"; // output prompt
+                                saveAToken(token);
+                                lineNum++;
+                                std::cout << "\n2> "; // input prompt
                             }
                         }
-                        else {
-                            //
+                        else { // ' ' or '\t'
+                            if (token.value[0] != '\"') { // token finished, store and reset
+                                std::cout << "\n3> " << token.value << "\n"; // output prompt
+                                saveAToken(token);
+                                columnNum++; // the position of white space
+                                std::cout << "\n4> "; // input prompt
+                            }
+                            else { // store into double-quote
+                                token.value += ch;
+                                columnNum++;
+                            }
                         }
                     }
                 }
@@ -279,12 +276,24 @@ class S_Exp_Lexer {
                     //
                 }
                 else if (ch == '\"' && ! token.value.empty()) {
-                    if (token.value[0] == '\"') {
+                    if (token.value[0] == '\"') { // end of double-quote
                         token.value += ch;
+                        std::cout << "\n5> " << token.value << "\n"; // output prompt
                         saveAToken(token);
-                        columnNum = 0;
+                        /*
+                        test cases:
+                        > ""
+                        > "123"  f "v
+                        */
+                        std::cout << "\n6> "; // input prompt
                     }
-                    // else throw NoClosingQuote(token.line, token.column, token.value);
+                    else {
+                        std::cout << "\n7> " << token.value << "\n"; // output prompt
+                        saveAToken(token); // store the token before double-quote, ex. > 4"
+                        token.value += ch;
+                        columnNum++;
+                        std::cout << "\n8> "; // input prompt
+                    }
                 }
                 else if (ch == '\'') {
                     //
@@ -295,93 +304,12 @@ class S_Exp_Lexer {
                 else if (ch == '+' || ch == '-' || ch == '.') {
                     //
                 }
-                else token.value += ch;
+                else {
+                    token.value += ch;
+                    columnNum++;
+                }
             }
             throw CorrectExit();
-        }
-
-        // deprecate
-        void readAndTokenize() {
-            std::cout << "> ";
-            Token token;
-            lineNum++;
-            ch = '\0';
-            prev_ch = '\0';
-
-            // avoid invisible char
-            // TODO: error must fix 'c' + Ctrl+Z + 'c' -> "cc", should be "c" "c"
-            if (std::cin.peek() == EOF) {
-                std::cin.clear();
-                throw NoMoreInput();
-            }
-
-            while (std::cin.get(ch)) {
-                columnNum++;
-                if (isWhiteSpace(ch)) {
-                    if (ch == '\n') {
-                        if (token.value[0] == '\"') throw NoClosingQuote(token.line, token.column);
-                        else {
-                            if (token.value != "") saveAToken(token);
-                            std::cout << "> "; // have bug when empty line or start with ;
-                            lineNum++;
-                            columnNum = 0;
-                        }
-                        prev_ch = ch;
-                        continue;
-                    }
-                    else if (token.value == "") {
-                        prev_ch = ch;
-                        continue; // don't save whitespace
-                    }
-                    else if (token.value != "" && token.value[0] != '\"') {
-                        saveAToken(token); // if whitespace not in double-qupte, skip it
-                        prev_ch = ch;
-                        continue;
-                    }
-                    // else DON'T continue, it may store into a double-quote
-                }
-                if (ch == ';') {
-                    if (! token.value.empty()) saveAToken(token);
-                    if (! tokens.empty() || token.value != "") lineNum++;
-                    columnNum = 0;
-                    while (std::cin.peek() != '\n') std::cin.get(ch); // skip the rest of the line
-                    prev_ch = ch;
-                    continue;
-                }
-                if (ch == '\\') { // escape
-                    char next_ch = std::cin.peek();
-
-                }
-                if (ch == '(' || ch == ')' || ch == '\'') {
-                    if (token.value == "") {
-                        prev_ch = ch;
-                        token.value += ch;
-                        saveAToken(token);
-                        continue;
-                    }
-                    else if (token.value[0] != '\"') {
-                        // save the token before '(' or ')'
-                        saveAToken(token);
-                        // save '(' or ')' as a single token
-                        prev_ch = ch;
-                        token.value += ch;
-                        saveAToken(token);
-                        continue;
-                    }
-                }
-
-                prev_ch = ch;
-                token.value += ch;
-
-                // end of a double-quote
-                if (token.value.length() >= 2 && token.value[0] == '\"' && token.value[token.value.length() - 1] == '\"') saveAToken(token);
-
-                // record token's position by the first char in token.value
-                if (token.line == -1 && token.column == -1) token.setPos(lineNum, columnNum);
-            }
-
-            saveAToken(token);
-            if (tokens.empty()) throw NoMoreInput();
         }
 };
 
@@ -391,15 +319,12 @@ class S_Exp_Parser {};
 /* Main Read-Eval-Print-Loop */
 int main() {
     std::getline(std::cin, gTestNum);
-    std::cout << "Welcome to OurScheme!\n" << std::endl;
+    std::cout << "Welcome to OurScheme!" << std::endl;
     S_Exp_Lexer lexer;
     S_Exp_Parser parser;
     while (true) {
         try {
             lexer.read();
-            //std::cout << "out" << std::endl;
-            //lexer.readAndTokenize();
-            //lexer.printAllTokens();
         } catch (CorrectExit &c) {
             std::cout << c.what();
             break;
